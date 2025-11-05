ff033bd2472e8aa9903911ae4acc82957218e42d
Arnav Pingle <anpingle@kth.se>
2025-11-05 18:17:58 +0100

    finished interpreter

--- interpreter.java
+++ interpreter.java
@@ -27,31 +27,24 @@ public class interpreter{
 
         for (String line : lines) {
             int end = line.indexOf("//");
-            if (end ==-1) {
-                line = line.strip();
-            }
-            else {
-                line = line.substring(0,end).strip();
-            }
-            fixedLines.add(line);
+            // if (line != "\n" || line!=" "){
+                if (end ==-1) {
+                    line = line.strip();
+                }
+                else {
+                    line = line.substring(0,end).strip();
+                }
+                fixedLines.add(line);
+            // }
         }
+        // for (int i = 0; i < fixedLines.size(); i++) {
+        //     if (fixedLines.get(i)==" "){
+        //         fixedLines.remove(i);
+        //     }
+        // }
         
         return fixedLines;
-    }
-
-    public void read_input() {
-        Scanner sc = new Scanner(System.in);
-        registers[1] = sc.nextInt();
-    }
-
-    public int parse_r_type(String str) {
-        int x = str.strip("#");
-        return x;
-    }
-
-    
-                
-            //ArrayList<String> words = line.split(" ");
+                    //ArrayList<String> words = line.split(" ");
             //int index = 0;
             //for (String s : words) {
 
@@ -70,11 +63,79 @@ public class interpreter{
                 index ++;
                 */
     
+    }
+
+    public void read_input() {
+        Scanner sc = new Scanner(System.in);
+        registers[1] = sc.nextInt();
+    }
+
+    public int removehash(String str) {
+        String[] x = str.split("#");
+        int y = Integer.parseInt(x[1]);
+        return y;
+    }
+
+    public void parse_instructions(ArrayList<String> instructions){
+        int i = 0;
+        while (i < instructions.size()){
+            String[] nospace = instructions.get(i).split(" ");
+            String instruction = nospace[0];
+            if(instruction.equals("input")){
+                read_input();
+            }
+            else if (instruction.equals("exit")){
+                break;
+            }
+            else if (instruction.equals("print")){
+                System.out.println(registers[1]);
+            }
+            else if (instruction.equals("add")){
+                int r1 = removehash(nospace[1]);
+                int r2 = removehash(nospace[2]);
+                int imm = Integer.parseInt(nospace[3]);
+                registers[r1] = registers[r1] + registers[r2] + imm;
+            }
+            else if (instruction.equals("sub")){
+                int r1 = removehash(nospace[1]);
+                int r2 = removehash(nospace[2]);
+                int imm = Integer.parseInt(nospace[3]);
+                registers[r1] = registers[r1] - registers[r2] - imm;
+
+            }
+            else if (instruction.equals("set")){
+                int r1 = removehash(nospace[1]);
+                int r2 = removehash(nospace[2]);
+                int imm = Integer.parseInt(nospace[3]);
+                registers[r1] = registers[r2] + imm;
+
+            }
+            else if (instruction.equals("j")){
+                i+=Integer.parseInt(nospace[1]);
+            }
+            else if (instruction.equals("jeq")){
+                int r1 = removehash(nospace[1]);
+                int r2 = removehash(nospace[2]);
+                int imm = Integer.parseInt(nospace[3]);
+                if (registers[r1]==registers[r2]){
+                    i+=imm;
+                }
+                else{
+                    if (imm==0){
+                        i+=1;
+                    }
+                }
+            }
+            i++;
+        }
+    }
+
     public static void main(String[] args) {
         interpreter test = new interpreter();
         ArrayList<String> k = test.fileline("example.bbvv");
         System.out.println(k);
         ArrayList<String> p = test.read_instructions(k);
         System.out.println(p);
+        test.parse_instructions(p);
     }
 }
